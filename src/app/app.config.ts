13c13c87102b96f62059c9a3954ae1c9a817a827
Alessio MENGHINI <alessiomenghini.dev@outlook.it>
2025-04-11 11:23:13 +0200

    Add Graphics Libraries

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeng/themes/material';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync("animations"),
+    providePrimeNG({
+      theme: {
+        preset: Material
+      }
+    })
+  ]
 };
