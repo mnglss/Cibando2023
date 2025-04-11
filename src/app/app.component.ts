5272ba52642acf504afdd3df8e5c037af493109f
Alessio MENGHINI <alessiomenghini.dev@outlook.it>
2025-04-11 12:54:05 +0200

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,32 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Cibando2023';
+
+  nomeFoto = 'foto del mare con le onde';
+  label = 'mare toscana';
+
+  urlFoto = 'https://i.etsystatic.com/51401719/r/il/43761f/6113922330/il_300x300.6113922330_l3jz.jpg';
+
+  images = [
+    {
+      Id: 1,
+      Label: 'Spaghetti al sugo'
+    },
+    {
+      Id: 2,
+      Label: 'Tagliata di manzo'
+    },
+    {
+      Id: 3,
+      Label: 'Tiramisù'
+    }
+  ];
+
+  percorsoFoto = 'assets/images/';
+
+  scriviLog() {
+    console.log('Hai cliccato col mouse sopra la foto!');
+  }
+
+
 }
