b8ba21efd0f00624ec38b5810235004b526e83d8
Alessio MENGHINI <alessiomenghini.dev@outlook.it>
2025-04-11 16:12:46 +0200

    Add Carousel BootstrapNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,40 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'; // Usato per il two way binding nei form
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbCarouselModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Cibando2023';
 
-  nomeFoto = 'foto del mare con le onde';
-  label = 'mare toscana';
-
-  urlFoto = 'https://i.etsystatic.com/51401719/r/il/43761f/6113922330/il_300x300.6113922330_l3jz.jpg';
-
   images = [
     {
       Id: 1,
-      Label: 'Spaghetti al sugo'
+      Titolo: 'Spaghetti al sugo'
     },
     {
       Id: 2,
-      Label: 'Tagliata di manzo'
+      Titolo: 'Tagliata di manzo'
     },
     {
       Id: 3,
-      Label: 'Tiramisù'
+      Titolo: 'Tiramisù'
     }
   ];
 
   percorsoFoto = 'assets/images/';
 
-  scriviLog() {
-    console.log('Hai cliccato col mouse sopra la foto!');
+
+  coloreScelto = 'green';
+  colorePagina = 'gray';
+
+  cambiaColoreAllaPagina() {
+    this.colorePagina = this.coloreScelto;
   }
-
-
 }
