f28332677a8f2fe2dfbdbf288a051324130fd759
Alessio MENGHINI <alessiomenghini.dev@outlook.it>
2025-04-23 17:51:29 +0200

    Lezione 15 - C

--- src/app/components/recipes/new-recipe/new-recipe.component.ts
+++ src/app/components/recipes/new-recipe/new-recipe.component.ts
@@ -6,11 +6,13 @@ import { RecipeService } from '../../../services/recipe.service';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api'; // Importa il servizio per le notifiche
 /* import { Select } from 'primeng/select'; */
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 
 @Component({
   selector: 'app-new-recipe',
-  imports: [FormsModule, ReactiveFormsModule, ToastModule ],
+  imports: [FormsModule, ReactiveFormsModule, ToastModule, CKEditorModule],
   templateUrl: './new-recipe.component.html',
   styleUrl: './new-recipe.component.scss',
   providers: [MessageService]
@@ -25,7 +27,31 @@ export class NewRecipeComponent {
     { label: '5', value: 5 }
   ];
 
-
+  editor = ClassicEditor;
+  editorConfig = {
+    toolbar: [
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      'link',
+      'bulletedList',
+      'numberedList',
+      'blockQuote',
+      'insertTable',
+      '|',
+      'undo',
+      'redo'
+    ],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
+      ]
+    }
+  };
 
 
   form = new FormGroup({
